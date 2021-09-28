bb91d533c51214a122ea2526811f31bfc9809ad1
pankaj kumar <raipankaj150@gmail.com>
2021-09-28 20:48:00 +0530

    sorting
    sorting by price completed for best seller page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let best_seller_product = [{"name":"Delicate Facial Cleanser Kashmiri Saffron & Neem","tag":"BESTSELLER","size":"200 ml","review":"4.7/5 (140 REVIEWS)","price":"₹1,395.00","image":"images/8878_delicate_facial_cleanser_kashmiri_saffron_neem_200ml_fornt1.png"},
-    {"name":"Soundarya Radiance Cream With 24K Gold & SPF25","tag":"BESTSELLER","size":"50 g","review":"4.7/5 (119 REVIEWS)","price":"₹5,400.00","image":"images/8842_soundarya_radiance_cream_50g_front.png"},
-    {"name":"Hand Pounded Organic Fruit Scrub","tag":"BESTSELLER","size":"50 g","review":"4.8/5 (29 REVIEWS)","price":"₹1,950.00","image":"images/8854_facial_scrub_hand_pounded_fruit_scrub_50g_front.png"},
-    {"name":"Hair Care Ritual For Glossy Hair","tag":"BESTSELLER","size":"600 g","review":"4.8/5 (129 REVIEWS)","price":"₹4,500.00","image":"images/hair_care_ritual_for_glossy_hair.png"},
+let best_seller_product = [{"name":"Delicate Facial Cleanser Kashmiri Saffron & Neem","tag":"BESTSELLER","size":"200 ml","review":"4.7/5 (140 REVIEWS)","price":"1,395.00","image":"images/8878_delicate_facial_cleanser_kashmiri_saffron_neem_200ml_fornt1.png"},
+    {"name":"Soundarya Radiance Cream With 24K Gold & SPF25","tag":"BESTSELLER","size":"50 g","review":"4.7/5 (119 REVIEWS)","price":"5,400.00","image":"images/8842_soundarya_radiance_cream_50g_front.png"},
+    {"name":"Hand Pounded Organic Fruit Scrub","tag":"BESTSELLER","size":"50 g","review":"4.8/5 (29 REVIEWS)","price":"1,950.00","image":"images/8854_facial_scrub_hand_pounded_fruit_scrub_50g_front.png"},
+    {"name":"Hair Care Ritual For Glossy Hair","tag":"BESTSELLER","size":"600 g","review":"4.8/5 (129 REVIEWS)","price":"4,500.00","image":"images/hair_care_ritual_for_glossy_hair.png"},
     ];
 
     let best_product_info = document.getElementById("best_product_info");
@@ -25,7 +25,7 @@ let best_seller_product = [{"name":"Delicate Facial Cleanser Kashmiri Saffron & 
             review.textContent = product.review;
 
             let price = document.createElement("p");
-            price.textContent = product.price;
+            price.textContent = "₹"+product.price;
 
             let add_to_bag = document.createElement("button");
             add_to_bag.innerHTML="ADD TO BAG";
