6e26ffabaf72ed802aaf31c7dd9ed3c362354483
Basil Aslam <Basilaslamnp@gmail.com>
2021-09-30 16:44:32 +0530

    catogery carousel completed

--- script.js
+++ script.js
@@ -273,3 +273,30 @@ let cart_show = document.getElementById("cart_show");
     }
 
     create();
+
+    // Catogery Carousel
+
+    const nextIcon = '<img src="./right-arrow.svg" alt="right">';
+    const prevIcon = '<img src="./left-arrow.svg" alt="left" >';
+    
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      autoplay: true,
+      margin:10,
+      nav: true,
+      navText: [
+      prevIcon,
+      nextIcon
+    ],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:3
+          },
+          1000:{
+              items:5
+          }
+      }
+    })
